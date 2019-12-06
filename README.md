8f48c9fc6acb77c35df462e9fc78f9049ff30239
Raphael Benoit <raphmc@hotmail.com>
2019-12-06 14:25:37 +0100

    added description and thesis

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 
 This repositpry containts the code for my Master's thesis: the implementation of a decentralised application called **hypenergy**, a peer-to-peer marketplace for community-based local electricity markets.
 
-* The chaincode can be found in: `packages/energymarket-cc/src/`  
-* The unit tests are described in: `packages/energymarket-cc/tests/energymarket.final.spec.ts`
+* The full thesis can be downloaded [here](https://github.com/raphmc/thesis//tree/master/thesis/thesis.pdf).
+* The chaincode describing the transaction logic can be found in: [`packages/energymarket-cc/src`](https://github.com/raphmc/thesis/tree/master/packages/energymarket-cc/src).
+* Test scripts and unit tests can be found in: [`packages/energymarket-cc/tests`](https://github.com/raphmc/thesis/tree/master/packages/energymarket-cc/tests)
 
 
-## Prerequisites (must be installed)
-* Node version 8.15.0
-* Docker 18.09.2 (make sure installation contains docker-compose)
-* npm 6.4.1
-* nvm can be used to install and switch between different node versions
+## Prerequisites
+* `Node 8.15.0` (support for `node 10.x.x` was announced and could also work now)
+* `Docker 18.09.2` (make sure installation contains docker-compose)
+* `npm 6.4.1`
+* `nvm` can be used to install and switch between different node versions
 
 
 ## Start
@@ -22,6 +23,11 @@ This repositpry containts the code for my Master's thesis: the implementation of
 `npm start`
 * Create a market by invoking the createMarket transaction (if timeout, try again)  
 `hurl invoke energymarket energymarket_createMarket "{\"id\":\"MKT\",\"auctionTime\":900000,\"gridBuyPrice\":25,\"gridSellPrice\":5}" -o org1 -u user1`
-* Run the tests  
+* Run tests  
 `npm run test`
 
+
+## Play Around
+
+* Check out the ledger: [`http://localhost:5084`](http://localhost:5084)
+* A debugger can be connected at [`http://localhost:8888`](http://localhost:8888)
--- packages/energymarket-cc/tests/energymarket.spec.ts
+++ packages/energymarket-cc/tests/energymarket.spec.ts
@@ -36,181 +36,178 @@ describe('Energymarket', () => {
     ]);
   });
 
-  // it('should create a MarketParticipant and send first SmartMeterReading', async () => {
+  it('should create a MarketParticipant and send first SmartMeterReading', async () => {
 
-  //   const participant = new MarketParticipant({
-  //     id: uuid(),
-  //     name: "Anton",
-  //     is: 1,
-  //   });
-  //   console.log(participant);
+    const participant = new MarketParticipant({
+      id: uuid(),
+      is: ParticipantType.prosumer,
+    });
+    console.log(participant);
 
-  //   let numberOfReadings = 10;
-  //   let readings = new Array<SmartMeterReading>(numberOfReadings);
-  //   for (let i=0; i<numberOfReadings; i++) {
-  //     readings[i] = new SmartMeterReading('AUC' + (i+1).toString());
-  //     readings[i].auctionPeriod = (i+1).toString();
-  //     readings[i].consumed = Math.floor(Math.random() * 100) + 1;
-  //     readings[i].produced = Math.floor(Math.random() * 100) + 1;
-  //   }
-  //   console.log(readings);
+    let numberOfReadings = 10;
+    let readings = new Array<SmartMeterReading>(numberOfReadings);
+    for (let i=0; i<numberOfReadings; i++) {
+      readings[i] = new SmartMeterReading('AUC' + (i+1).toString());
+      readings[i].auctionPeriod = (i+1).toString();
+      readings[i].consumed = Math.floor(Math.random() * 100) + 1;
+      readings[i].produced = Math.floor(Math.random() * 100) + 1;
+    }
+    console.log(readings);
 
-  //   await energymarketCtrl.createMarketParticipant(participant);
-  //   let savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
-  //   console.log(savedParticipant);
+    await energymarketCtrl.createMarketParticipant(participant);
+    let savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
+    console.log(savedParticipant);
 
-  //   for (const reading of readings) { await energymarketCtrl.sendReading(reading, participant.id) };
-  //   // await energymarketCtrl.sendReading(data, participant.id);
-  //   savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
-  //   console.log(savedParticipant);
+    for (const reading of readings) { await energymarketCtrl.sendReading(reading) };
+    // await energymarketCtrl.sendReading(data, participant.id);
+    savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
+    console.log(savedParticipant);
 
-  //   expect(savedParticipant.id).to.exist;
-  //   expect(savedParticipant.readings).to.be.an('array').lengthOf(numberOfReadings);
-  // });
+    expect(savedParticipant.id).to.exist;
+    expect(savedParticipant.readings).to.be.an('array').lengthOf(numberOfReadings);
+  });
   
-  // it('should create a market', async () => {
-  //   const market = new Market({
-  //     id: uuid(),
-  //     gridBuyPrice: 28,
-  //     gridSellPrice: 5,
-  //     auctionTime: 900000
-  //   });
+  it('should create a market', async () => {
+    const market = new Market({
+      id: uuid(),
+      gridBuyPrice: 28,
+      gridSellPrice: 5,
+      auctionTime: 900000
+    });
     
-  //   await energymarketCtrl.createMarket(market);
-  //   const savedMarket = await adapter.getById<Market>(market.id);
-  //   console.log(savedMarket);
-  //   expect(savedMarket.id).to.exist;
-  // });
+    await energymarketCtrl.createMarket(market);
+    const savedMarket = await adapter.getById<Market>(market.id);
+    console.log(savedMarket);
+    expect(savedMarket.id).to.exist;
+  });
 
-  // it('should create a participant, an auction, a bid and an ask', async () => {
+  it('should create a participant, an auction, a bid and an ask', async () => {
 
-  //   const participant = new MarketParticipant({
-  //     id: "B6:0B:37:7C:DF:D2:7A:08:0B:98:BF:52:A4:2C:DC:4E:CC:70:91:E1",
-  //     name: "Anton",
-  //     is: 1,
-  //     coinBalance: 100000000000
-  //   });
-  //   console.log(participant);
+    const participant = new MarketParticipant({
+      id: "B6:0B:37:7C:DF:D2:7A:08:0B:98:BF:52:A4:2C:DC:4E:CC:70:91:E1",
+      is: ParticipantType.prosumer
+    });
+    console.log(participant);
 
-  //   await energymarketCtrl.createMarketParticipant(participant);
+    await energymarketCtrl.createMarketParticipant(participant);
     
 
-  //   const auction = new Auction({
-  //     id: uuid(),
-  //     start: Date.now(),
-  //     end: (Date.now() + 100000),
-  //   });
+    const auction = new Auction({
+      id: uuid(),
+      start: Date.now(),
+      end: (Date.now() + 100000),
+    });
 
-  //   await energymarketCtrl.createAuction(auction);
-  //   let savedAuction = await adapter.getById<Auction>(auction.id);
-  //   console.log(savedAuction);
+    await energymarketCtrl.createAuction(auction);
+    let savedAuction = await adapter.getById<Auction>(auction.id);
+    console.log(savedAuction);
 
-  //   let numberOfBids = 10;
-  //   let bids = new Array<Bid>(numberOfBids)
-  //   for (let i=0; i<numberOfBids; i++) {
-  //     bids[i] = new Bid({ id: uuid(), auctionId: auction.id, amount: Math.floor(Math.random() * 100) + 1, price: Math.floor(Math.random() * 20) + 5});
-  //   }
-  //   for (const bid of bids) { await energymarketCtrl.placeBid(bid)};
-  //   // await Promise.all(bids.map(bid => energymarketCtrl.placeBid(bid)));
-  //   let savedBids = await energymarketCtrl.getAllBids().then(bids => bids.map(bid => new Bid(bid)));
+    let numberOfBids = 10;
+    let bids = new Array<Bid>(numberOfBids)
+    for (let i=0; i<numberOfBids; i++) {
+      bids[i] = new Bid({ id: uuid(), auctionId: auction.id, amount: Math.floor(Math.random() * 100) + 1, price: Math.floor(Math.random() * 20) + 5});
+    }
+    for (const bid of bids) { await energymarketCtrl.placeBid(bid)};
+    // await Promise.all(bids.map(bid => energymarketCtrl.placeBid(bid)));
+    let savedBids = await energymarketCtrl.getAllBids().then(bids => bids.map(bid => new Bid(bid)));
     
-  //   // const bid = new Bid({
-  //   //   id: uuid(),
-  //   //   auctionId: auction.id,
-  //   //   amount: 20,
-  //   //   price: 10,
-  //   //   sender: "this is the string of the sender",
-  //   // })
+    // const bid = new Bid({
+    //   id: uuid(),
+    //   auctionId: auction.id,
+    //   amount: 20,
+    //   price: 10,
+    //   sender: "this is the string of the sender",
+    // })
 
-  //   // const bid2 = new Bid({
-  //   //   id: uuid(),
-  //   //   auctionId: auction.id,
-  //   //   amount: 30,
-  //   //   price: 15,
-  //   //   sender: "another sender"
-  //   // })
+    // const bid2 = new Bid({
+    //   id: uuid(),
+    //   auctionId: auction.id,
+    //   amount: 30,
+    //   price: 15,
+    //   sender: "another sender"
+    // })
     
-  //   // await energymarketCtrl.placeBid(bid);
-  //   // await energymarketCtrl.placeBid(bid2);
+    // await energymarketCtrl.placeBid(bid);
+    // await energymarketCtrl.placeBid(bid2);
   
-  //   let numberOfAsks = 10;
-  //   let asks = new Array<Ask>(numberOfAsks)
-  //   for (let i=0; i<numberOfAsks; i++) {
-  //     asks[i] = new Ask({ id: uuid(), auctionId: auction.id, amount: Math.floor(Math.random() * 100) + 1, price: Math.floor(Math.random() * 20) + 5});
-  //   }
-  //   for (const ask of asks) { await energymarketCtrl.placeAsk(ask)};
-  //   let savedAsks = await energymarketCtrl.getAllAsks().then(asks => asks.map(ask => new Ask(ask)));
+    let numberOfAsks = 10;
+    let asks = new Array<Ask>(numberOfAsks)
+    for (let i=0; i<numberOfAsks; i++) {
+      asks[i] = new Ask({ id: uuid(), auctionId: auction.id, amount: Math.floor(Math.random() * 100) + 1, price: Math.floor(Math.random() * 20) + 5});
+    }
+    for (const ask of asks) { await energymarketCtrl.placeAsk(ask)};
+    let savedAsks = await energymarketCtrl.getAllAsks().then(asks => asks.map(ask => new Ask(ask)));
 
-  //   // const ask = new Ask({
-  //   //   id: uuid(),
-  //   //   auctionId: auction.id,
-  //   //   amount: 50,
-  //   //   price: 15,
-  //   //   sender: "sender",
-  //   // })
+    // const ask = new Ask({
+    //   id: uuid(),
+    //   auctionId: auction.id,
+    //   amount: 50,
+    //   price: 15,
+    //   sender: "sender",
+    // })
 
-  //   // const ask2 = new Ask({
-  //   //   id: uuid(),
-  //   //   auctionId: auction.id,
-  //   //   amount: 10,
-  //   //   price: 20,
-  //   //   sender: "sender",
-  //   // })
+    // const ask2 = new Ask({
+    //   id: uuid(),
+    //   auctionId: auction.id,
+    //   amount: 10,
+    //   price: 20,
+    //   sender: "sender",
+    // })
 
-  //   // await energymarketCtrl.placeAsk(ask);
-  //   // await energymarketCtrl.placeAsk(ask2);
+    // await energymarketCtrl.placeAsk(ask);
+    // await energymarketCtrl.placeAsk(ask2);
 
     
 
-  //   /** Clear the auction */
-  //   await energymarketCtrl.clearAuction(auction.id).catch(ex => ex.responses[0].error.message);
+    /** Clear the auction */
+    await energymarketCtrl.clearAuction(auction.id).catch(ex => ex.responses[0].error.message);
 
-  //   // let numberOfReadings = 10;
-  //   // let readings = new Array<SmartMeterReading>(numberOfReadings);
-  //   // for (let i=0; i<numberOfReadings; i++) {
-  //   //   readings[i] = new SmartMeterReading('AUC' + (i+1).toString());
-  //   //   readings[i].auctionPeriod = (i+1).toString();
-  //   //   readings[i].consumed = Math.floor(Math.random() * 100) + 1;
-  //   //   readings[i].produced = Math.floor(Math.random() * 100) + 1;
-  //   // }
-  //   // console.log(readings);
-  //   // for (const reading of readings) { await energymarketCtrl.sendReading(reading, participant.id) };
+    // let numberOfReadings = 10;
+    // let readings = new Array<SmartMeterReading>(numberOfReadings);
+    // for (let i=0; i<numberOfReadings; i++) {
+    //   readings[i] = new SmartMeterReading('AUC' + (i+1).toString());
+    //   readings[i].auctionPeriod = (i+1).toString();
+    //   readings[i].consumed = Math.floor(Math.random() * 100) + 1;
+    //   readings[i].produced = Math.floor(Math.random() * 100) + 1;
+    // }
+    // console.log(readings);
+    // for (const reading of readings) { await energymarketCtrl.sendReading(reading, participant.id) };
 
 
 
-  //   savedAuction = await adapter.getById<Auction>(auction.id);
-  //   console.log(savedAuction);
-  //   savedBids = await energymarketCtrl.getAllBids().then(bids => bids.map(bid => new Bid(bid)));
-  //   console.log(savedBids);
-  //   savedAsks = await energymarketCtrl.getAllAsks().then(asks => asks.map(ask => new Ask(ask)));
-  //   console.log(savedAsks);
-  //   const savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
-  //   console.log(savedParticipant);
+    savedAuction = await adapter.getById<Auction>(auction.id);
+    console.log(savedAuction);
+    savedBids = await energymarketCtrl.getAllBids().then(bids => bids.map(bid => new Bid(bid)));
+    console.log(savedBids);
+    savedAsks = await energymarketCtrl.getAllAsks().then(asks => asks.map(ask => new Ask(ask)));
+    console.log(savedAsks);
+    const savedParticipant = await adapter.getById<MarketParticipant>(participant.id);
+    console.log(savedParticipant);
 
-  //   let sumDemand = savedAuction.matchedAmount + savedAuction.unmatchedDemand;
-  //   let sumSupply = savedAuction.matchedAmount + savedAuction.unmatchedSupply;
-  //   let sumBids = bids.reduce( (acc, bid) => acc += bid.amount, 0);
-  //   let sumAsks = asks.reduce( (acc, ask) => acc += ask.amount, 0);
+    let sumDemand = savedAuction.matchedAmount + savedAuction.unmatchedDemand;
+    let sumSupply = savedAuction.matchedAmount + savedAuction.unmatchedSupply;
+    let sumBids = bids.reduce( (acc, bid) => acc += bid.amount, 0);
+    let sumAsks = asks.reduce( (acc, ask) => acc += ask.amount, 0);
 
-  //   let edge: number;
-  //   for (const element of [...savedBids, ...savedAsks]) {
-  //     if (element.unmatchedAmount) {edge = element.price};
-  //   }
-  //   console.log(`Bid or Ask on the edge is ${edge} and a MCP of ${savedAuction.mcp}`);
+    let edge: number;
+    for (const element of [...savedBids, ...savedAsks]) {
+      if (element.unmatchedAmount) {edge = element.price};
+    }
+    console.log(`Bid or Ask on the edge is ${edge} and a MCP of ${savedAuction.mcp}`);
 
-  //   // expect(savedAuction.matchedAmount).to.be.eql(30);
-  //   expect(edge).to.be.eql(savedAuction.mcp);
-  //   expect(savedAuction.mcp).to.exist;
-  //   expect(savedParticipant.id).to.exist;
-  //   expect(savedBids).to.be.an('array').that.has.lengthOf(numberOfBids);
-  //   savedBids.forEach(element => expect(element).to.have.deep.property('id'));
-  //   // expect(savedBids[1]).to.have.property('successful', true);
-  //   expect(savedAsks).to.be.an('array').that.has.lengthOf(numberOfAsks);
-  //   savedAsks.forEach(element => expect(element).to.have.property('id'));
-  //   expect(savedAsks[0]).to.have.deep.property('unmatchedAmount');
-  //   expect(sumDemand).to.be.eql(sumBids);
-  //   expect(sumSupply).to.be.eql(sumAsks);
-  // });
+    // expect(savedAuction.matchedAmount).to.be.eql(30);
+    expect(edge).to.be.eql(savedAuction.mcp);
+    expect(savedAuction.mcp).to.exist;
+    expect(savedParticipant.id).to.exist;
+    expect(savedBids).to.be.an('array').that.has.lengthOf(numberOfBids);
+    savedBids.forEach(element => expect(element).to.have.deep.property('id'));
+    // expect(savedBids[1]).to.have.property('successful', true);
+    expect(savedAsks).to.be.an('array').that.has.lengthOf(numberOfAsks);
+    savedAsks.forEach(element => expect(element).to.have.property('id'));
+    expect(savedAsks[0]).to.have.deep.property('unmatchedAmount');
+    expect(sumDemand).to.be.eql(sumBids);
+    expect(sumSupply).to.be.eql(sumAsks);
+  });
 
 
 
